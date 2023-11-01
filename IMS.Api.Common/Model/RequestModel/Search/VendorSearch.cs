66857aa66a8c1e294a19430fa47ea5b56410f8ee
Muhammad Fahad <Muh.Fahad29@gmail.com>
2023-11-01 21:14:07 +0400

    Vendor API Complete

--- IMS.Api.Common/Model/RequestModel/Search/VendorSearch.cs
+++ IMS.Api.Common/Model/RequestModel/Search/VendorSearch.cs
@@ -1,11 +1,13 @@
-﻿using IMS.Api.Common.Model.CommonModel;
-
-namespace IMS.Api.Common.Model.RequestModel.Search
+﻿namespace IMS.Api.Common.Model.RequestModel.Search
 {
-    public class VendorSearch : BaseFilter
+    public class VendorSearch
     {
+        public int VendorId { get; set; } = -1;
+        public int CompanyId { get; set; } = -1;
+        public string VendorCompany { get; set; } = string.Empty;
         public string PhoneNo { get; set; } = string.Empty;
-        public string IDCardNo { get; set; } = string.Empty;
+        public string IdCardNo { get; set; } = string.Empty;
+        public int? PageNo { get; set; } = 1;
+        public int? RecordLimit { get; set; } = 100;
     }
-
 }
